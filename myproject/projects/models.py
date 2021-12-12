797a4241768d068d22a0b43d2f49c3a4bfb9493b
Plamen Iliev <pakounicredit@gmail.com>
2021-12-12 16:04:21 +0100

    Django

--- myproject/projects/models.py
+++ myproject/projects/models.py
@@ -3,4 +3,7 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    
