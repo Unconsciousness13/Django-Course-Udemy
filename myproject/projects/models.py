e69af52b878709c6cdc73350855395bcf13747c6
Plamen Iliev <pakounicredit@gmail.com>
2021-12-12 16:07:53 +0100

    Django

--- myproject/projects/models.py
+++ myproject/projects/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid
 
 
 class Project(models.Model):
@@ -6,4 +7,6 @@ class Project(models.Model):
     description = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
-    
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
